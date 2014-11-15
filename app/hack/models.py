4064eb6d4a7ae77207253f7358bcf49525402604
Wan Kim Mok <cafe.mui@gmail.com>
2014-11-14 23:45:48 -0500

    modified AddClass for students to be many-to-many relation

--- app/hack/models.py
+++ app/hack/models.py
@@ -3,11 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class AddClass(models.Model):
-	class_name = models.CharField(max_length=200)
 
-	def __unicode__(self):
-		return self.class_name
 
 class StudentInfo(models.Model):
 	number = models.IntegerField(default=0)
@@ -15,3 +11,12 @@ class StudentInfo(models.Model):
 
 	def __unicode__(self):
 		return self.email
+
+
+
+class AddClass(models.Model):
+	class_name = models.CharField(max_length=200)
+	students = models.ManyToManyField(StudentInfo)
+
+	def __unicode__(self):
+		return self.class_name
