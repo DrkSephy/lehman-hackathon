a7c4d53f5229e4ab22e507507b4c22b6c3bb263b
David Leonard <sephirothcloud1025@yahoo.com>
2014-11-14 22:05:31 -0500

    Adding admin.py to register Student Info and AddClass Models

--- app/hack/admin.py
+++ app/hack/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from hack.models import AddClass
+from hack.models import StudentInfo
 
 # Register your models here.
+
+class AddClassAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['class_name']})
+    ]
+
+class StudentInfoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['number']}),
+        (None, {'fields': ['email']})
+    ]
+
+admin.site.register(AddClass, AddClassAdmin)
+admin.site.register(StudentInfo, StudentInfoAdmin)
