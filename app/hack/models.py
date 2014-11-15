8a742f38def3a749dbdef701716f580af3e57e82
Wan Kim Mok <cafe.mui@gmail.com>
2014-11-14 21:25:26 -0500

    created models AddClass and StudentInfo

--- app/hack/models.py
+++ app/hack/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class AddClass(models.Model):
+	class_name = models.CharField(maxlength=200)
+
+	def __unicode__(self):
+		return self.class_name
+
+class StudentInfo(models.Model):
+	number = models.IntegerField(default=0)
+	email = models.CharField(maxlength=200)
+
+	def __unicode__(self):
+		return self.email, self.number
