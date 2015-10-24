00834bd16415c3e1416e3fc4bfd4550ef9920a26
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-24 18:42:55 -0400

    Minor updates

--- twilio-david/send_call.py
+++ twilio-david/send_call.py
@@ -1,15 +1,15 @@
 from twilio.rest import TwilioRestClient 
  
 # put your own credentials here 
-ACCOUNT_SID = "AC7889a1889c1833bd7181e45e60372776" 
-AUTH_TOKEN = "1ad0315f3cc7a154aaaef048f1304f71" 
+ACCOUNT_SID = "" 
+AUTH_TOKEN = "" 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
 call = client.calls.create(
-    to="+13473282978", 
-    from_="+13477323709",  
-    application_sid="AC7889a1889c1833bd7181e45e60372776", 
+    to="", 
+    from_="",  
+    application_sid="", 
     method="GET",  
     fallback_method="GET",  
     status_callback_method="GET",    
